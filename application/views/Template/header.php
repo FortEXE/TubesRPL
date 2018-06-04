7fc408df6dc7b243f30a2794d6c1d8f912fe59a0
Adnan Khairi <squadron296@gmail.com>
2018-06-05 01:54:31 +0700

    Fix merge

--- application/views/Template/header.php
+++ application/views/Template/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title><?= $page_title ?></title>
+	<link rel="icon" href="<?php echo base_url("img/samsung.png"); ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #EEEEEE">
 
